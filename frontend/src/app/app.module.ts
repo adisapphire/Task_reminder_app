d077592d5ede5068d1710057be13637e322dca9f
Puneet Bindal <puneet.bindal@maximl.com>
2019-05-17 12:04:10 +0530

    initialized components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { TaskComponent } from './task/task.component';
+import { TaskDetailComponent } from './task-detail/task-detail.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    TaskComponent,
+    TaskDetailComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/models/user.ts
@@ -0,0 +1,7 @@
+export class User {
+    id: number;
+    Name: string;
+    username: string;
+    password: string;
+    token?: string;
+}
