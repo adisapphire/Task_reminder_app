12f6b8971dd20382aab9ccd8928ac05ab500e79e
ujjawal.mishra <you@example.com>
2019-05-20 15:48:51 +0530

    registartion updated

---
+++ backend/api/register/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+
+# Create your views here.
+
+class UserView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    http_method_names = ['post', 'head']
+
+
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response("Error in posting")
+
+        
+class UserApiView(generics.RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
