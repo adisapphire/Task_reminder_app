4296961fb86b963a12b83e7905a2ebe7a7a5fbdb
Puneet Bindal <puneet.bindal@maximl.com>
2019-05-23 15:07:58 +0530

    removed unwanted files

---
+++ frontend/src/app/urlsettings.ts
@@ -0,0 +1,13 @@
+
+export const Urlsettings =  {
+  LOCALHOST: 'http://192.168.20.87:8000',
+  AuthURL: '/api/token/',
+  AUTHREFRESHURL: 'refresh/',
+  TasksUrl: '/api/tasks/',
+  TaskshowUrl: 'show',
+  UserUrl: '/api/users/',
+  POSTUrl: 'add',
+  WSheader: 'ws://192.168.20.87:8000',
+  WebsocketURL: '/ws/foobar?subscribe-broadcast'
+}
+
